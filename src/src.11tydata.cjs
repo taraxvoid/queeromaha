2981d1f8c8f666763f1804e32e1547405c666035
void <1711810+v01dpointer@users.noreply.github.com>
2026-05-21 11:55:24 -0500

    attempt to fix login to decapcms

--- src/src.11tydata.cjs
+++ src/src.11tydata.cjs
@@ -3,8 +3,8 @@ module.exports = {
     eleventyExcludeFromCollections: (data) => data.public === false,
     permalink: (data) => {
       if (data.public === false) return false;
-      // preserve explicit permalink: false set by directory data files
       if (data.permalink === false) return false;
+      if (data.permalink) return data.permalink;
     },
   },
 };
