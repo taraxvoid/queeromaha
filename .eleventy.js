cb1ff0216ca1c762ea57ff1fe51d738c35d315bd
Tara \X/ Void <1711810+taraxvoid@users.noreply.github.com>
2026-05-31 21:40:49 -0500

    open links in new tab

--- .eleventy.js
+++ .eleventy.js
@@ -6,6 +6,23 @@ module.exports = function (eleventyConfig) {
 
   let markdownLibrary;
   eleventyConfig.amendLibrary("md", (mdLib) => {
+    // Add rel="noopener noreferrer" to external links
+    const ext_link =
+      mdLib.renderer.rules.link_open ||
+      ((tokens, idx) => mdLib.utils.escapeHtml(tokens[idx].content));
+
+    mdLib.renderer.rules.link_open = (tokens, idx, options, env, renderer) => {
+      const token = tokens[idx];
+      const href = token.attrGet("href");
+
+      // Check if link is external
+      if (href && !href.startsWith("/") && !href.startsWith("#")) {
+        token.attrSet("rel", "noopener noreferrer");
+      }
+
+      return ext_link(tokens, idx, options, env, renderer);
+    };
+
     markdownLibrary = mdLib;
   });
 
